26a711ca0cf2ee4bbdc74cbcdf6bc35aec0d16b4
marenel07 <villaralbonel29@gmail.com>
2022-12-12 19:20:50 +0930

    event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,31 @@
 const socket = io()
 
+
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -22,6 +38,8 @@ document.querySelector('#send-location').addEventListener('click', () => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
         })
     })
 })
