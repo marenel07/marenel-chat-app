6346d924435e483acf133caf91b0a2d69b6775b9
marenel07 <villaralbonel29@gmail.com>
2022-12-16 22:00:47 +0930

    socket io rooms

--- src/index.js
+++ src/index.js
@@ -20,8 +20,12 @@ let count = 0
 io.on('connection', (socket) => {
     console.log('New websocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    })
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -31,7 +35,7 @@ io.on('connection', (socket) => {
         }
 
 
-        io.emit('message', generateMessage(message))
+        io.to('Center City').emit('message', generateMessage(message))
         callback()
     })
 
