a895c75a978a9d150f41b3e15789b8d0a0367e45
marenel07 <villaralbonel29@gmail.com>
2022-12-16 22:57:23 +0930

    adding function about storing and removing user

---
+++ src/utils/users.js
@@ -0,0 +1,49 @@
+const users = []
+
+
+
+const addUser = ({ id, username, room}) => {
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if(existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id ===id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+addUser({
+    id: 23,
+    username: 'Marenel',
+    room: '    Center City'
+})
+
+console.log(users)
+
+const removedUser = removeUser(23)
+
+console.log(removedUser)
+console.log(users)
