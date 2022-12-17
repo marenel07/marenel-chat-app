7d21c02f9262ff18bcafb30480397ee24a8eb84b
marenel07 <villaralbonel29@gmail.com>
2022-12-17 23:06:49 +0930

    change style.css file

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
 
         if(filter.isProfane(message)) {
@@ -44,12 +45,13 @@ io.on('connection', (socket) => {
         }
 
 
-        io.to('Center City').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
